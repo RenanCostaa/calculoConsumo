78e044c0a7a70846fe9370850bcf54e65294d6a8
PC_UCL <pc@ucl.br>
2022-10-31 20:09:27 -0300

    comitando atualizacao

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
+from dis import dis
 from flask import Flask, render_template, request, redirect
+import _json
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def index():
-    return render_template('index.html')
+class veiculo:    
+    @app.route("/", methods=['GET','POST'])
+    def index():
+        dict = {}
+        try:
+            print("AQUI")
+            if request.method == 'POST':
+                print("TO AQUI")
+                carro_usuario = request.form['input_name_car']
+                km_liter = request.form['input_km_ltr']
+                dist_viagem = request.form['input_dist_viagem']
+                dict = {
+                    "CARRO" : carro_usuario,
+                    "KM/L" : km_liter,
+                    "DISTANCIA": dist_viagem
+                }
+        except:
+            print("toaquiexcept")
+            pass
+
+        
+        print(dict) 
+        return render_template('index.html')
 
 
 if app == __name__:
