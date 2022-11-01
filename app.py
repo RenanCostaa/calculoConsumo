6c4f71fd39f536bdbfc686c826c7dd3d2e41ab86
Arthur Zimmermann de Oliveira <arthurzimmermann@ucl.br>
2022-10-31 23:39:05 -0300

    update

--- app.py
+++ app.py
@@ -7,28 +7,29 @@ app = Flask(__name__)
 
 class veiculo:    
 
-   # def calcula():
-        #logging.info("enter calcula")
-        
-        
-
-
     @app.route("/", methods=['GET','POST'])
     def index():  
         dict = {}
         try:
             gasolina = json.loads(open('combustiveis.json','r').readlines()[0])
             print(gasolina)
+            print(gasolina['gasolina']['31/10'])
             logging.info("validacao request GET/POST")
             if request.method == 'POST':
                 logging.info("is Post")
                 carro_usuario = request.form['input_name_car']
                 km_liter = float(request.form['input_km_ltr'])
                 dist_viagem = float(request.form['input_dist_viagem'])
+                date = request.form['input_date']
+                valor_dia = float(gasolina['gasolina'][date])
+                calculo = (dist_viagem/km_liter) * valor_dia
                 dict = {
                     "CARRO" : carro_usuario,
                     "KM/L" : km_liter,
-                    "DISTANCIA": dist_viagem
+                    "DISTANCIA": dist_viagem,
+                    "DATE" : date,
+                    "VALOR DO DIA" : valor_dia,
+                    "CALCULO" : round(calculo,2)
                 }
 
         except:
@@ -36,7 +37,7 @@ class veiculo:
             pass
 
         print(dict) 
-        return render_template('index.html')
+        return render_template('index.html', dict = dict)
 
     
 
